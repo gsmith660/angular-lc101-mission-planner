6c6c7f1805c63c514cfa5fe5f025f18c5e538829
gsmith660 <gsmith660@gmail.com>
2019-12-14 16:57:59 -0600

    can now add, remove, and edit equipment list

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipmentList: string[] = [
+    'Habitat dome',
+    'Drones',
+    'Food containers',
+    'Oxygen tanks'
+  ]
+  itemBeingEdited = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(newItem: string) {
+    this.equipmentList.push(newItem);
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(newItem: string, item: string) {
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList.splice(index, 1, newItem);
+    this.itemBeingEdited = null;
+  }
+
+  remove(item: string) {
+    let index = this.equipmentList.indexOf(item);
+    this.equipmentList.splice(index, 1);
   }
 
 }
